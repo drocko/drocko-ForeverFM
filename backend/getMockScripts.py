653fc836cc523551154eca1059e2547208fc7c14
Anthony Silva <ajsilva228@outlook.com>
2025-04-05 20:16:41 -0700

    Moved script files to separate folder; fixed names in mock scripts

--- backend/getMockScripts.py
+++ backend/getMockScripts.py
@@ -13,7 +13,7 @@ for i in range(8):
     new_script = generateContent.generateContent(scripts, conv_topic)
     scripts.append(new_script)
     file_name = f'''mock_script{i}.json'''
-    file_path = os.path.join('./mock_data',file_name)
+    file_path = os.path.join('./mock_data./scripts',file_name)
     with open(file_path, 'w') as json_file:
         json.dump(new_script, json_file, indent=4)  # indent=4 makes the JSON more readable
     time.sleep(8)
